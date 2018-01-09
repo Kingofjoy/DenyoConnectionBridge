006d45aede505aa532cf88f93ec59cddbf24d238
kingofjoy <kingofjoy@live.in>
2018-01-10 01:19:06 +0530

    Server Update

--- ConnectionBridge/ConnectionBridgeServer/TCPServer/TCPServer/Server.cs
+++ ConnectionBridge/ConnectionBridgeServer/TCPServer/TCPServer/Server.cs
@@ -15,35 +15,39 @@ using System.Collections.Concurrent;
 namespace Denyo.ConnectionBridge.Server.TCPServer
 {
 
-    public static class Server
+    public class Server
     {
-        static TcpListener serverSocket = null;
+        TcpListener serverSocket = null;
 
-        static ConcurrentBag<Client> myClients = new ConcurrentBag<Client>();
+        ConcurrentBag<Client> Clients = new ConcurrentBag<Client>();
 
-        static DateTime dtNext;
-        static bool bHBInit = false;
+        ConcurrentQueue<DataPacket> ReceivedMessages = new ConcurrentQueue<DataPacket>();
 
-        public static bool bAcceptLoop = false;
+        DateTime dtNext;
+        bool bHBInit = false;
 
-        public static int ListenerPort { get { return _ListenerPort; } }
-        static int _ListenerPort;
+        public bool bAcceptLoop = false;
 
-        public static bool IsHeartBeatActive = false;
-        static int HeartBeatInterval;
+        public bool bReceiveLoop = false;
 
-        static public bool IsActive { get; set; }
+        public int ListenerPort { get { return _ListenerPort; } }
+        int _ListenerPort;
 
-        static public string AppID { get; set; }
+        public bool IsHeartBeatActive = false;
+        int HeartBeatInterval;
 
-        static public string AppType { get; set; }
+        public bool IsActive { get; set; }
 
-        static public string AuthToken { get; set; }
+        public string AppID { get; set; }
+
+        public string AppType { get; set; }
+
+        public string AuthToken { get; set; }
 
 
-        static Server()
+        Server()
         {
-           try
+            try
             {
                 IsActive = false;
 
@@ -58,13 +62,13 @@ namespace Denyo.ConnectionBridge.Server.TCPServer
 
                 _ListenerPort = int.Parse(ConfigurationManager.AppSettings["SLPort"]);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-
+                Log(ex, "Server");
             }
         }
 
-        public static bool Start()
+        public bool Start()
         {
             try
             {
@@ -72,205 +76,299 @@ namespace Denyo.ConnectionBridge.Server.TCPServer
                 serverSocket.Start();
 
                 IsActive = true;
-            }
-            catch(Exception ex)
-            {
 
+                bAcceptLoop = true;
+            }
+            catch (Exception ex)
+            {
+                Log(ex, "Start");
             }
             return IsActive;
         }
 
-        public static void AcceptIncomingConnetion()
+        public void AcceptIncomingConnetion()
         {
             Byte[] ByteData;
 
             try
             {
-                while (serverSocket.Pending())
+                while (bAcceptLoop)
                 {
-                    Client myClient = new Client();
-                    myClient.Instance = serverSocket.AcceptTcpClient();
-                    myClient.Stream = myClient.Instance.GetStream();
-
                     bool bSend, bReceive, bPacketSend, bPacketReceive;
                     bSend = bReceive = bPacketSend = bPacketReceive = false;
-                    string data=string.Empty;
-                    DataPacket dpTest=new DataPacket();
+                    string data = string.Empty;
+                    DataPacket dpTest = new DataPacket();
 
-                    //Send Test
-                    #region Init_Send_Test
-
-                    ByteData = Encoding.ASCII.GetBytes("SHB " + DateTime.Now.ToString("dd MM yyyy HH mm ss") + "$");
-
-                    for (int i = 0; i < 2; i++)
+                    while (serverSocket.Pending())
                     {
-                        try
+                        Log("Incoming new client request");
+                        Client myClient = new Client();
+                        myClient.Instance = serverSocket.AcceptTcpClient();
+                        myClient.Stream = myClient.Instance.GetStream();
+
+                        //Send Test
+                        #region Init_Send_Test
+
+                        ByteData = Encoding.ASCII.GetBytes(DateTime.Now.ToString("dd MM yyyy HH mm ss") + "$");
+
+                        for (int i = 0; i < 2; i++)
                         {
-                            if (!myClient.Instance.Connected || !myClient.Stream.CanWrite)
-                                for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
-
-                            myClient.Stream.Write(ByteData, 0, ByteData.Length);
-                            myClient.Stream.Flush();
-
-                            bSend = true;
-
-                        }
-                        catch { }
-
-                        if(!bSend)
-                            for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
-                    }
-
-                    #endregion
-
-                    //Receive Test
-                    #region Init_Receive_Test
-
-                    for (int i = 0; i < 2; i++)
-                    {
-                        try
-                        {
-                            if (myClient.Instance.Connected && myClient.Instance.Available > 0 && !bReceive)
+                            try
                             {
+                                if (!myClient.Instance.Connected || !myClient.Stream.CanWrite)
+                                    for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
 
-                                ByteData = new byte[myClient.Instance.ReceiveBufferSize];
-                                int recb = myClient.Stream.Read(ByteData, 0, myClient.Instance.ReceiveBufferSize);
+                                myClient.Stream.Write(ByteData, 0, ByteData.Length);
+                                myClient.Stream.Flush();
 
-                                if (recb == 0) continue; 
+                                bSend = true;
 
-                                data = System.Text.Encoding.ASCII.GetString(ByteData);
-
-                                if(data.Length>0)
-                                    bReceive = true;
                             }
+                            catch { }
 
-                        }
-                        catch { }
-
-                        if (!bReceive)
-                            for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
-                    }
-
-                    #endregion
-
-                    //Send Packet Test
-                    #region Init_SendPacket_Test
-
-                    for (int i = 0; i < 2; i++)
-                    {
-
-                        dpTest = new DataPacket();
-                        dpTest.Message = "Test Data";
-                        dpTest.SenderID = AppID;
-                        dpTest.SenderType = DataStructures.AppType.Server;
-                        dpTest.RecepientID = "Unknown";
-                        dpTest.RecepientType = DataStructures.AppType.Client;
-                        dpTest.TimeStamp = DateTime.Now;
-
-                        if (bPacketSend) continue;
-
-                        try
-                        {
-                            data = JsonConvert.SerializeObject(dpTest);
-                            ByteData = Encoding.ASCII.GetBytes(data);
-
-                            if (!myClient.Instance.Connected || !myClient.Stream.CanWrite)
+                            if (!bSend)
                                 for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
-
-                            myClient.Stream.Write(ByteData, 0, ByteData.Length);
-                            myClient.Stream.Flush();
-
-                            bPacketSend = true;
-                        }
-                        catch (Exception spEx)
-                        {
-                            bPacketSend = false;
-
                         }
 
-                        if (!bPacketSend)
-                            for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
+                        #endregion
 
-                    }
-                    #endregion
+                        //Receive Test
+                        #region Init_Receive_Test
 
-                    //Receive Packet Test
-                    #region Init_ReceivePacketTest
-
-                    for (int i = 0; i < 2; i++)
-                    {
-
-                        if (bPacketReceive) continue;
-
-                        try
+                        for (int i = 0; i < 2; i++)
                         {
-                            if (myClient.Instance.Connected && myClient.Instance.Available > 0)
+                            try
                             {
-
-                                ByteData = new byte[myClient.Instance.ReceiveBufferSize];
-                                int recb = myClient.Stream.Read(ByteData, 0, myClient.Instance.ReceiveBufferSize);
-
-                                if (recb == 0) continue;
-
-                                data = System.Text.Encoding.ASCII.GetString(ByteData);
-
-                                if (!(data.Length > 0))
+                                if (myClient.Instance.Connected && myClient.Instance.Available > 0 && !bReceive)
                                 {
-                                    bPacketReceive = false;
-                                    continue;
+
+                                    ByteData = new byte[myClient.Instance.ReceiveBufferSize];
+                                    int recb = myClient.Stream.Read(ByteData, 0, myClient.Instance.ReceiveBufferSize);
+
+                                    if (recb == 0) continue;
+
+                                    data = System.Text.Encoding.ASCII.GetString(ByteData);
+
+                                    if (data.Length > 0)
+                                        bReceive = true;
                                 }
 
-                                dpTest = new DataPacket();
-                                dpTest = JsonConvert.DeserializeObject<DataPacket>(data);
+                            }
+                            catch { }
 
-                                if (dpTest.SenderID != string.Empty)
-                                {
-                                    myClient.Name = dpTest.SenderID;
-                                    myClient.Type = dpTest.SenderType;
-                                    myClient.LastActive = DateTime.Now;
+                            if (!bReceive)
+                                for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
+                        }
 
-                                    bPacketReceive = true;
-                                }
+                        #endregion
+
+                        //Send Packet Test
+                        #region Init_SendPacket_Test
+
+                        for (int i = 0; i < 2; i++)
+                        {
+
+                            dpTest = new DataPacket();
+                            dpTest.Message = "Test Data";
+                            dpTest.SenderID = AppID;
+                            dpTest.SenderType = DataStructures.AppType.Server;
+                            dpTest.RecepientID = "Unknown";
+                            dpTest.RecepientType = DataStructures.AppType.Client;
+                            dpTest.TimeStamp = DateTime.Now;
+
+                            if (bPacketSend) continue;
+
+                            try
+                            {
+                                data = JsonConvert.SerializeObject(dpTest);
+                                ByteData = Encoding.ASCII.GetBytes(data);
+
+                                if (!myClient.Instance.Connected || !myClient.Stream.CanWrite)
+                                    for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
+
+                                myClient.Stream.Write(ByteData, 0, ByteData.Length);
+                                myClient.Stream.Flush();
+
+                                bPacketSend = true;
+                            }
+                            catch (Exception spEx)
+                            {
+                                bPacketSend = false;
+
                             }
 
-                        }
-                        catch {
-                            bPacketReceive = false;
-                        }
+                            if (!bPacketSend)
+                                for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
 
-                        if (!bPacketReceive)
-                            for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
-                    }
-                    #endregion
+                        }
+                        #endregion
 
-                    if(myClient.Name.Length>0)
-                    {
-                        myClients.Add(myClient);
+                        //Receive Packet Test
+                        #region Init_ReceivePacketTest
+
+                        for (int i = 0; i < 2; i++)
+                        {
+
+                            if (bPacketReceive) continue;
+
+                            try
+                            {
+                                if (myClient.Instance.Connected && myClient.Instance.Available > 0)
+                                {
+
+                                    ByteData = new byte[myClient.Instance.ReceiveBufferSize];
+                                    int recb = myClient.Stream.Read(ByteData, 0, myClient.Instance.ReceiveBufferSize);
+
+                                    if (recb == 0) continue;
+
+                                    data = System.Text.Encoding.ASCII.GetString(ByteData);
+
+                                    if (!(data.Length > 0))
+                                    {
+                                        bPacketReceive = false;
+                                        continue;
+                                    }
+
+                                    dpTest = new DataPacket();
+                                    dpTest = JsonConvert.DeserializeObject<DataPacket>(data);
+
+                                    if (dpTest.SenderID != string.Empty)
+                                    {
+                                        myClient.Name = dpTest.SenderID;
+                                        myClient.Type = dpTest.SenderType;
+                                        myClient.LastActive = DateTime.Now;
+
+                                        bPacketReceive = true;
+                                    }
+                                }
+
+                            }
+                            catch
+                            {
+                                bPacketReceive = false;
+                            }
+
+                            if (!bPacketReceive)
+                                for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
+                        }
+                        #endregion
+
+                        if (myClient.Name.Length > 0)
+                        {
+                            Clients.Add(myClient);
+                            bReceiveLoop = true;
+                            Log("New Client " + myClient.Name + " Added from " + myClient.Instance.Client.LocalEndPoint);
+                        }
+                        else
+                        {
+                            try {
+                                Log("Unable to accept client request. [" + myClient.Name + " , " + myClient.Instance.Client.LocalEndPoint + ", Send: " + bSend + ", Receive: " + bReceive + ", PacketSend: " + bPacketSend + ", bPacketSend: " + bPacketSend + "]");
+                            }
+                            catch
+                            {
+                                Log("Unable to accept client request. [" + myClient.Name + " ,  , Send: " + bSend + ", Receive: " + bReceive + ", PacketSend: " + bPacketSend + ", bPacketSend: " + bPacketSend + "]");
+                            }
+                        }
                     }
                 }
             }
-            catch(Exception ICex)
+            catch (Exception ICex)
             {
 
             }
         }
 
-        static void Log(string Message)
+        public void ReceiveMessages()
         {
+            try
+            {
+                while(bReceiveLoop)
+                {
+                    foreach (Client cliobj in Clients)
+                    {
+                        while (cliobj.Instance.Connected && cliobj.Instance.Available > 0)
+                        {
+
+                            try
+                            {
+                                byte[] bytesFrom = new byte[cliobj.Instance.ReceiveBufferSize];
+
+                                int recb = cliobj.Stream.Read(bytesFrom, 0, (int)cliobj.Instance.ReceiveBufferSize);
+
+                                if (recb == 0) continue;
+
+                                string data = System.Text.Encoding.ASCII.GetString(bytesFrom);
+
+                                if (data[0] == '\0') continue ;
+
+                                DataPacket dpReceived = new DataPacket();
+                                dpReceived  = JsonConvert.DeserializeObject<DataPacket>(data);
+
+                                ReceivedMessages.Enqueue(dpReceived);
+                            }
+                            catch (Exception ex2)
+                            {
+                                Log(ex2, "ReceiveMsg:Er:[" + cliobj.Name + "]");
+                                //todo: check if client still connected
+                            }
+
+                                
+
+                        }
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Log(ex, "ReceiveMessages");
+            }
 
         }
 
-        static void Log(Exception ex,string Message="")
-        {
 
+
+        public bool SendMessage(DataPacket dpToSend)
+        {
+            bool iSMsgSent = false;
+
+            try
+            {
+                Client TargetClient = Clients.FirstOrDefault(x => (x.Name == dpToSend.RecepientID));
+                if(TargetClient.Instance.Connected)
+                {
+                    if ((TargetClient.Instance.Available > 0))
+                        for (int j = 0; j <= int.MaxValue; j++) { /* give a small delay */ }
+
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(dpToSend));
+                    TargetClient.Stream.Write(sendBytes, 0, sendBytes.Length);
+                    TargetClient.Stream.Flush();
+                }
+            }
+            catch(Exception ex)
+            {
+                Log(ex, "Error on SendMessage" )
+            }
+
+            return iSMsgSent;
+        }
+        void Log(string Message)
+        {
+            Console.WriteLine(DateTime.Now.ToString("ddMMyyyy HHmmss") + " : " + Message);
         }
 
-        static void Log(DataPacket packet)
+        void Log(Exception ex, string Message = "")
+        {
+            Console.WriteLine(DateTime.Now.ToString("ddMMyyyy HHmmss") + " : Error : " + Message + " > " + ex.Message);
+            Console.WriteLine(DateTime.Now.ToString("ddMMyyyy HHmmss") + " : Error Trace : " + ex.StackTrace);
+        }
+
+        void Log(DataPacket packet)
         {
 
         }
 
     }
 
-    
+
 }
